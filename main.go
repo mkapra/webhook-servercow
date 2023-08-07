127d2b7831350f3f5b71e2ea62d4d4718f9e8d77
Maximilian Kapra <maximilian@kapra.de>
2023-08-07 22:38:52 +0200

    Adjust main.go for servercow API

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
+	"regexp"
+
+	"net/http"
 
 	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
@@ -19,114 +26,133 @@ func main() {
 		panic("GROUP_NAME must be specified")
 	}
 
-	// This will register our custom DNS provider with the webhook serving
-	// library, making it available as an API under the provided GroupName.
-	// You can register multiple DNS provider implementations with a single
-	// webhook, where the Name() method will be used to disambiguate between
-	// the different implementations.
 	cmd.RunWebhookServer(GroupName,
 		&customDNSProviderSolver{},
 	)
 }
 
-// customDNSProviderSolver implements the provider-specific logic needed to
-// 'present' an ACME challenge TXT record for your own DNS provider.
-// To do so, it must implement the `github.com/cert-manager/cert-manager/pkg/acme/webhook.Solver`
-// interface.
 type customDNSProviderSolver struct {
-	// If a Kubernetes 'clientset' is needed, you must:
-	// 1. uncomment the additional `client` field in this structure below
-	// 2. uncomment the "k8s.io/client-go/kubernetes" import at the top of the file
-	// 3. uncomment the relevant code in the Initialize method below
-	// 4. ensure your webhook's service account has the required RBAC role
-	//    assigned to it for interacting with the Kubernetes APIs you need.
-	//client kubernetes.Clientset
+	client *kubernetes.Clientset
 }
 
-// customDNSProviderConfig is a structure that is used to decode into when
-// solving a DNS01 challenge.
-// This information is provided by cert-manager, and may be a reference to
-// additional configuration that's needed to solve the challenge for this
-// particular certificate or issuer.
-// This typically includes references to Secret resources containing DNS
-// provider credentials, in cases where a 'multi-tenant' DNS solver is being
-// created.
-// If you do *not* require per-issuer or per-certificate configuration to be
-// provided to your webhook, you can skip decoding altogether in favour of
-// using CLI flags or similar to provide configuration.
-// You should not include sensitive information here. If credentials need to
-// be used by your provider here, you should reference a Kubernetes Secret
-// resource and fetch these credentials using a Kubernetes clientset.
 type customDNSProviderConfig struct {
-	// Change the two fields below according to the format of the configuration
-	// to be decoded.
 	// These fields will be set by users in the
 	// `issuer.spec.acme.dns01.providers.webhook.config` field.
-
-	//Email           string `json:"email"`
-	//APIKeySecretRef v1alpha1.SecretKeySelector `json:"apiKeySecretRef"`
+	UserPasswordSecretRef string `json:"secretRef"`
+	NamespaceRef          string `json:"secretNamespace"`
 }
 
-// Name is used as the name for this DNS solver when referencing it on the ACME
-// Issuer resource.
-// This should be unique **within the group name**, i.e. you can have two
-// solvers configured with the same Name() **so long as they do not co-exist
-// within a single webhook deployment**.
-// For example, `cloudflare` may be used as the name of a solver.
 func (c *customDNSProviderSolver) Name() string {
-	return "my-custom-solver"
+	return "servercow"
 }
 
-// Present is responsible for actually presenting the DNS record with the
-// DNS provider.
-// This method should tolerate being called multiple times with the same value.
-// cert-manager itself will later perform a self check to ensure that the
-// solver has correctly configured the DNS provider.
 func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
 	cfg, err := loadConfig(ch.Config)
 	if err != nil {
 		return err
 	}
 
-	// TODO: do something more useful with the decoded configuration
-	fmt.Printf("Decoded configuration %v", cfg)
+	userName, userPassword, err := c.getConfig(&cfg, ch)
+	if err != nil {
+		return fmt.Errorf("Could not retrieve username and password from secret: %v", err)
+	}
 
-	// TODO: add code that sets a record in the DNS provider's console
+	rePattern := regexp.MustCompile(`^(.+)\.(([^\.]+)\.([^\.]+))\.$`)
+	match := rePattern.FindStringSubmatch(ch.ResolvedFQDN)
+	if match == nil {
+		return fmt.Errorf("unable to parse host/domain out of resolved FQDN ('%s')", ch.ResolvedFQDN)
+	}
+	host := match[1]
+	domain := match[2]
+
+	url := fmt.Sprintf("https://api.servercow.de/dns/v1/domains/%s", domain)
+	payload := map[string]interface{}{
+		"type":    "TXT",
+		"name":    host,
+		"content": ch.Key,
+		"ttl":     20,
+	}
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("X-Auth-Username", userName)
+	req.Header.Set("X-Auth-Password", userPassword)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
 	return nil
 }
 
-// CleanUp should delete the relevant TXT record from the DNS provider console.
-// If multiple TXT records exist with the same record name (e.g.
-// _acme-challenge.example.com) then **only** the record with the same `key`
-// value provided on the ChallengeRequest should be cleaned up.
-// This is in order to facilitate multiple DNS validations for the same domain
-// concurrently.
 func (c *customDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
-	// TODO: add code that deletes a record from the DNS provider's console
+	cfg, err := loadConfig(ch.Config)
+	if err != nil {
+		return err
+	}
+
+	userName, userPassword, err := c.getConfig(&cfg, ch)
+	if err != nil {
+		return fmt.Errorf("Could not retrieve username and password from secret: %v", err)
+	}
+
+	rePattern := regexp.MustCompile(`^(.+)\.(([^\.]+)\.([^\.]+))\.$`)
+	match := rePattern.FindStringSubmatch(ch.ResolvedFQDN)
+	if match == nil {
+		return fmt.Errorf("unable to parse host/domain out of resolved FQDN ('%s')", ch.ResolvedFQDN)
+	}
+	host := match[1]
+	domain := match[2]
+
+	url := fmt.Sprintf("https://api.servercow.de/dns/v1/domains/%v", domain)
+	payload := map[string]interface{}{
+		"type": "TXT",
+		"name": host,
+	}
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("DELETE", url, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("X-Auth-Username", userName)
+	req.Header.Set("X-Auth-Password", userPassword)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
 	return nil
 }
 
 // Initialize will be called when the webhook first starts.
-// This method can be used to instantiate the webhook, i.e. initialising
-// connections or warming up caches.
-// Typically, the kubeClientConfig parameter is used to build a Kubernetes
-// client that can be used to fetch resources from the Kubernetes API, e.g.
-// Secret resources containing credentials used to authenticate with DNS
-// provider accounts.
-// The stopCh can be used to handle early termination of the webhook, in cases
-// where a SIGTERM or similar signal is sent to the webhook process.
 func (c *customDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
-	///// UNCOMMENT THE BELOW CODE TO MAKE A KUBERNETES CLIENTSET AVAILABLE TO
-	///// YOUR CUSTOM DNS PROVIDER
+	cl, err := kubernetes.NewForConfig(kubeClientConfig)
+	if err != nil {
+		return err
+	}
 
-	//cl, err := kubernetes.NewForConfig(kubeClientConfig)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//c.client = cl
+	c.client = cl
 
-	///// END OF CODE TO MAKE KUBERNETES CLIENTSET AVAILABLE
 	return nil
 }
 
@@ -143,4 +169,38 @@ func loadConfig(cfgJSON *extapi.JSON) (customDNSProviderConfig, error) {
 	}
 
 	return cfg, nil
+}
+
+func stringFromSecretData(secretData *map[string][]byte, key string) (string, error) {
+	data, ok := (*secretData)[key]
+	if !ok {
+		return "", fmt.Errorf("key %q not found in secret data", key)
+	}
+	return string(data), nil
+}
+
+func (n *customDNSProviderSolver) getConfig(cfg *customDNSProviderConfig, ch *v1alpha1.ChallengeRequest) (string, string, error) {
+	var secretNs string
+	if cfg.NamespaceRef != "" {
+		secretNs = cfg.NamespaceRef
+	} else {
+		secretNs = ch.ResourceNamespace
+	}
+
+	secret, err := n.client.CoreV1().Secrets(secretNs).Get(context.TODO(), cfg.UserPasswordSecretRef, metav1.GetOptions{})
+	if err != nil {
+		return "", "", fmt.Errorf("unable to get secret %s/%s': %v", secretNs, cfg.UserPasswordSecretRef, err)
+	}
+
+	userName, err := stringFromSecretData(&secret.Data, "username")
+	if err != nil {
+		return "", "", fmt.Errorf("unable to get username for user from secret: %v", err)
+	}
+
+	userPassword, err := stringFromSecretData(&secret.Data, "password")
+	if err != nil {
+		return "", "", fmt.Errorf("unable to get password for user from secret: %v", err)
+	}
+
+	return userName, userPassword, nil
 }
